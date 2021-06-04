67f812b2709b490da74f82a032cc8717a65ef19b
Mirella Flores <mire_flor3@hotmail.com>
2021-06-04 12:06:08 -0400

    fix

--- lib/SGN/Controller/OpenidConfiguration.pm
+++ lib/SGN/Controller/OpenidConfiguration.pm
@@ -1,7 +1,6 @@
 
 package SGN::Controller::OpenidConfiguration;
 
-use File::Slurp;
 use Moose;
 
 BEGIN { extends "Catalyst::Controller"; }
@@ -12,11 +11,8 @@ sub openid_configuration : Path('/.well-known/openid-configuration') Args(0) {
     my $self = shift;
     my $c = shift;
 
-    my $json = read_file($c->get_conf('basepath') . '/static/documents/openid-configuration.json');
-    my $site = $c->config->{main_production_site_url};
-    $json =~ s/\<\% \$site \%>/$site/; 
-    $c->res->content_type("application/json");
-    $c->res->body($json);
+    $c->stash->{site} =  $c->config->{main_production_site_url};
+    $c->stash->{template} = '/well-known/openid-configuration.txt';
 }
 
 
