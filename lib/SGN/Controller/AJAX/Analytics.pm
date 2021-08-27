7e5c0d12e20eec9e15d4272abac0d047d09f9de1
nickmorales <nmorales3142@gmail.com>
2021-08-27 11:24:38 -0400

    debug

--- lib/SGN/Controller/AJAX/Analytics.pm
+++ lib/SGN/Controller/AJAX/Analytics.pm
@@ -2458,6 +2458,7 @@ sub analytics_protocols_compare_to_trait :Path('/ajax/analytics_protocols_compar
     if ($tolparinv_10 eq '0.1' || $tolparinv eq '0.1' || $tolparinv eq '0.2' || $tolparinv eq '0.5') {
         $tol_asr = 'c(-1,-2)';
     }
+    $tol_asr = 'c(-1,-2)';
 
     my $number_traits = scalar(@sorted_trait_names);
     my $number_accessions = scalar(@accession_ids);
