09ddf6eacbafb2f5727f63ff829a038a7be31b35
nickmorales <nmorales3142@gmail.com>
2022-01-26 18:02:39 -0500

    debug

--- lib/SGN/Controller/AJAX/DroneImagery/DroneImageryAnalytics.pm
+++ lib/SGN/Controller/AJAX/DroneImagery/DroneImageryAnalytics.pm
@@ -4155,14 +4155,16 @@ sub drone_imagery_calculate_analytics_POST : Args(0) {
                     push @$spatial_effects_files_store, [$full_plot_level_correlation_tempfile, "datafile_".$statistics_select.$sim_env_change_over_time.$correlation_between_times."_fullcorr_"."envvar_".$env_variance_percent."_".$permanent_environment_structure."_".$iterations];
                 };
 
-                # print STDERR Dumper \@sorted_trait_names;
-                # print STDERR Dumper \%$trait_to_time_map_hash_1;
-                # print STDERR Dumper $rr_unique_traits_hash_1;
-                #
-                # print STDERR Dumper $result_blup_data_original_1->{$stock_name_row_col{$unique_plot_names[0]}->{germplasm_name}};
-                # print STDERR Dumper $phenotype_data_original{$unique_plot_names[0]};
-                # print STDERR Dumper $result_blup_pe_data_delta_original_1->{$unique_plot_names[0]};
-                # print STDERR Dumper $result_residual_data_original_1->{$unique_plot_names[0]};
+                print STDERR Dumper \@sorted_trait_names;
+                print STDERR Dumper \%$trait_to_time_map_hash_1;
+                print STDERR Dumper $rr_unique_traits_hash_1;
+
+                print STDERR Dumper $result_blup_data_original_1->{$stock_name_row_col{$unique_plot_names[0]}->{germplasm_name}};
+                print STDERR Dumper $phenotype_data_original{$unique_plot_names[0]};
+                print STDERR Dumper $result_blup_pe_data_delta_original_1->{$unique_plot_names[0]};
+                print STDERR Dumper $result_blup_data_delta_original_1->{$unique_plot_names[0]};
+                print STDERR Dumper $result_residual_data_original_1->{$unique_plot_names[0]};
+                die;
 
                 eval {
                     my ($plot_level_original_values_tempfile_fh, $plot_level_original_values_tempfile) = tempfile("drone_stats_XXXXX", DIR=> $tmp_stats_dir);
