f8915ef513b81b855cced137e4fc918df7f31674
nickmorales <nmorales3142@gmail.com>
2022-03-04 17:45:19 -0500

    editing company

---
+++ lib/SGN/Controller/AJAX/Company.pm
@@ -0,0 +1,143 @@
+
+=head1 NAME
+
+SGN::Controller::AJAX::Company - a REST controller class to provide the
+backend for managing company
+
+=head1 DESCRIPTION
+
+
+=cut
+
+package SGN::Controller::AJAX::Company;
+
+use Moose;
+use Data::Dumper;
+use Try::Tiny;
+use JSON;
+use SGN::Model::Cvterm;
+use CXGN::PrivateCompany;
+
+BEGIN { extends 'Catalyst::Controller::REST' }
+
+__PACKAGE__->config(
+    default   => 'application/json',
+    stash_key => 'rest',
+    map       => { 'application/json' => 'JSON', 'text/html' => 'JSON'  },
+);
+
+sub add_company : Path('/ajax/private_company/create_company') : ActionClass('REST') { }
+sub add_company_POST : Args(0) {
+    my ($self, $c) = @_;
+    my $schema = $c->dbic_schema("Bio::Chado::Schema");
+
+    if (!$c->user()) {
+        $c->stash->{rest} = {error => 'Please login to add a new company!' };
+        return;
+    }
+    my $sp_person_id = $c->user()->get_object()->get_sp_person_id();
+
+    my $private_company = CXGN::PrivateCompany->new({
+        schema=> $schema,
+        sp_person_id => $sp_person_id,
+        private_company_name => $c->req->param('name'),
+        private_company_description => $c->req->param('description'),
+        private_company_contact_email => $c->req->param('contact_email'),
+        private_company_contact_person_first_name => $c->req->param('contact_first_name'),
+        private_company_contact_person_last_name => $c->req->param('contact_last_name'),
+        private_company_contact_person_phone => $c->req->param('contact_phone'),
+        private_company_address_street => $c->req->param('address_street'),
+        private_company_address_street_2 => $c->req->param('address_street_2'),
+        private_company_address_state => $c->req->param('address_state'),
+        private_company_address_city => $c->req->param('address_city'),
+        private_company_address_zipcode => $c->req->param('address_zipcode'),
+        private_company_address_country => $c->req->param('address_country'),
+        private_company_create_date => $c->req->param('contact_email'),
+        private_company_type_cvterm_id => SGN::Model::Cvterm->get_cvterm_row($schema, $c->req->param('company_access_type'), 'company_type')->cvterm_id()
+    });
+    my $return = $private_company->store_private_company();
+
+    $c->stash->{rest} = $return;
+}
+
+sub edit_company : Path('/ajax/private_company/edit_company') : ActionClass('REST') { }
+sub edit_company_POST : Args(0) {
+    my ($self, $c) = @_;
+    my $schema = $c->dbic_schema("Bio::Chado::Schema");
+
+    if (!$c->user()) {
+        $c->stash->{rest} = {error => 'Please login to edit a company!' };
+        return;
+    }
+    my $sp_person_id = $c->user()->get_object()->get_sp_person_id();
+
+    my $private_company = CXGN::PrivateCompany->new({
+        schema=> $schema,
+        sp_person_id => $sp_person_id,
+        is_storing_or_editing => 1,
+        private_company_id => $c->req->param('private_company_id'),
+        private_company_name => $c->req->param('name'),
+        private_company_description => $c->req->param('description'),
+        private_company_contact_email => $c->req->param('contact_email'),
+        private_company_contact_person_first_name => $c->req->param('contact_first_name'),
+        private_company_contact_person_last_name => $c->req->param('contact_last_name'),
+        private_company_contact_person_phone => $c->req->param('contact_phone'),
+        private_company_address_street => $c->req->param('address_street'),
+        private_company_address_street_2 => $c->req->param('address_street_2'),
+        private_company_address_state => $c->req->param('address_state'),
+        private_company_address_city => $c->req->param('address_city'),
+        private_company_address_zipcode => $c->req->param('address_zipcode'),
+        private_company_address_country => $c->req->param('address_country'),
+        private_company_create_date => $c->req->param('contact_email'),
+        private_company_type_cvterm_id => SGN::Model::Cvterm->get_cvterm_row($schema, $c->req->param('company_access_type'), 'company_type')->cvterm_id()
+    });
+    my $return = $private_company->edit_private_company();
+
+    $c->stash->{rest} = $return;
+}
+
+sub add_company_member : Path('/ajax/private_company/add_company_member') : ActionClass('REST') { }
+sub add_company_member_POST : Args(0) {
+    my ($self, $c) = @_;
+    my $schema = $c->dbic_schema("Bio::Chado::Schema");
+
+    if (!$c->user()) {
+        $c->stash->{rest} = {error => 'Please login to add a company member!' };
+        return;
+    }
+    my $sp_person_id = $c->user()->get_object()->get_sp_person_id();
+
+    my $private_company = CXGN::PrivateCompany->new({
+        schema=> $schema,
+        sp_person_id => $sp_person_id,
+        is_storing_or_editing => 1,
+        private_company_id => $c->req->param('private_company_id'),
+    });
+    my $return = $private_company->add_private_company_member([$c->req->param('add_sp_person_id'), $c->req->param('company_person_access_type')]);
+
+    $c->stash->{rest} = $return;
+}
+
+sub remove_company_member : Path('/ajax/private_company/remove_company_member') : ActionClass('REST') { }
+sub remove_company_member_POST : Args(0) {
+    my ($self, $c) = @_;
+    my $schema = $c->dbic_schema("Bio::Chado::Schema");
+
+    if (!$c->user()) {
+        $c->stash->{rest} = {error => 'Please login to remove a company member!' };
+        return;
+    }
+    my $sp_person_id = $c->user()->get_object()->get_sp_person_id();
+
+    my $private_company = CXGN::PrivateCompany->new({
+        schema=> $schema,
+        sp_person_id => $sp_person_id,
+        is_storing_or_editing => 1,
+        private_company_id => $c->req->param('private_company_id'),
+    });
+    my $return = $private_company->remove_private_company_member($c->req->param('remove_sp_person_id'));
+
+    $c->stash->{rest} = $return;
+}
+
+1;
---
+++ lib/SGN/Controller/Company.pm
@@ -0,0 +1,77 @@
+package SGN::Controller::Company;
+
+use Moose;
+use Data::Dumper;
+use Try::Tiny;
+use SGN::Model::Cvterm;
+use Data::Dumper;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+sub company_page :Path("/company") Args(1) {
+    my $self = shift;
+    my $c = shift;
+    my $company_id = shift;
+    my $schema = $c->dbic_schema('Bio::Chado::Schema', 'sgn_chado');
+
+    if (!$c->user()) {
+        $c->res->redirect( uri( path => '/user/login', query => { goto_url => $c->req->uri->path_query } ) );
+        return;
+    }
+    my $sp_person_id = $c->user()->get_object()->get_sp_person_id();
+
+    my $private_company = CXGN::PrivateCompany->new({
+        schema=> $schema,
+        private_company_id => $company_id,
+        sp_person_id => $sp_person_id
+    });
+    my $name = $private_company->private_company_name();
+
+    if (!$name) {
+        $c->stash->{message} = "The requested company does not exist or has been deleted.";
+        $c->stash->{template} = 'generic_message.mas';
+        return;
+    }
+
+    my $description = $private_company->private_company_description();
+    my $contact_email = $private_company->private_company_contact_email();
+    my $contact_first_name = $private_company->private_company_contact_person_first_name();
+    my $contact_last_name = $private_company->private_company_contact_person_last_name();
+    my $contact_phone = $private_company->private_company_contact_person_phone();
+    my $address_street = $private_company->private_company_address_street();
+    my $address_street_2 = $private_company->private_company_address_street_2();
+    my $state = $private_company->private_company_address_state();
+    my $city = $private_company->private_company_address_city();
+    my $zipcode = $private_company->private_company_address_zipcode();
+    my $country = $private_company->private_company_address_country();
+    my $create_date = $private_company->private_company_create_date();
+    my $company_type_cvterm_id = $private_company->private_company_type_cvterm_id();
+    my $company_type_cvterm_name = $private_company->private_company_type_name();
+    my $sp_person_access_cvterm_id = $private_company->sp_person_access_cvterm_id();
+    my $sp_person_access_cvterm_name = $private_company->sp_person_access_cvterm_name();
+    my $company_members = $private_company->private_company_members();
+
+    $c->stash->{private_company_id} = $company_id;
+    $c->stash->{name} = $name;
+    $c->stash->{description} = $description;
+    $c->stash->{contact_email} = $contact_email;
+    $c->stash->{contact_first_name} = $contact_first_name;
+    $c->stash->{contact_last_name} = $contact_last_name;
+    $c->stash->{contact_phone} = $contact_phone;
+    $c->stash->{address_street} = $address_street;
+    $c->stash->{address_street_2} = $address_street_2;
+    $c->stash->{state} = $state;
+    $c->stash->{city} = $city;
+    $c->stash->{zipcode} = $zipcode;
+    $c->stash->{country} = $country;
+    $c->stash->{create_date} = $create_date;
+    $c->stash->{company_type_cvterm_id} = $company_type_cvterm_id;
+    $c->stash->{company_type_cvterm_name} = $company_type_cvterm_name;
+    $c->stash->{company_sp_person_access_cvterm_id} = $sp_person_access_cvterm_id;
+    $c->stash->{company_sp_person_access_cvterm_name} = $sp_person_access_cvterm_name;
+    $c->stash->{company_members} = $company_members;
+
+    $c->stash->{template} = '/breeders_toolbox/private_companies/private_company.mas';
+}
+
+1;
