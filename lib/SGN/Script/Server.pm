7c2a867a03eab7abd735f7f64988dd999514524c
nickmorales <nmorales3142@gmail.com>
2022-04-04 12:38:34 -0400

    Revert "adapt npm user to file ownership."
    This reverts commit 8e6c3a16e09979d0094a2dfd0616271324c049e3.

--- lib/SGN/Script/Server.pm
+++ lib/SGN/Script/Server.pm
@@ -8,9 +8,7 @@ extends 'Catalyst::Script::Server';
 
 if (@ARGV && "-r" ~~ @ARGV) {
     $ENV{SGN_WEBPACK_WATCH} = 1;
-    my $uid = (lstat("js/node_modules"))[4];
-    print STDERR "\n\nSGN_WEBPACK_WATCH: USING USER ID $uid FOR npm...\n\n\n";
-    system("cd js && sudo -u $uid npm run build-watch &");
+    system("cd js && npm run build-watch &");
 } 
 
 1;
