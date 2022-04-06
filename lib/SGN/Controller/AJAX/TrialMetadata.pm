5a9ffbd01b1510f6682b3f9b7b4cb55ed10f85fd
Mirella Flores <mire_flor3@hotmail.com>
2022-04-06 14:31:06 -0400

    merge

--- lib/SGN/Controller/AJAX/TrialMetadata.pm
+++ lib/SGN/Controller/AJAX/TrialMetadata.pm
@@ -3199,6 +3199,24 @@ sub genotyping_trial_from_field_trial : Chained('trial') PathPart('genotyping_tr
     $c->stash->{rest} = {success => 1, genotyping_trials_from_field_trial => $genotyping_trials_from_field_trial, field_trials_source_of_genotyping_trial => $field_trials_source_of_genotyping_trial};
 }
 
+sub delete_linkage_genotyping_plate_from_field_trial : Chained('trial') PathPart('delete_linkage_genotyping_plate_from_field_trial') Args(1) {
+    my $self = shift;
+    my $c = shift;
+    my $field_trial_id = shift;
+    my $schema = $c->dbic_schema("Bio::Chado::Schema");
+
+    my ($user_id, $user_name, $user_role) = _check_user_login_trial_metadata($c, 'curator', 'curator_access');
+
+    my $result = $c->stash->{trial}->delete_linkage_genotyping_plate_from_field_trial($field_trial_id, $user_role);
+
+    if (exists($result->{errors})) {
+        $c->stash->{rest} = { error => $result->{errors} };
+    }
+    else {
+        $c->stash->{rest} = { success => 1 };
+    }
+}
+
 sub crossing_trial_from_field_trial : Chained('trial') PathPart('crossing_trial_from_field_trial') Args(0) {
     my $self = shift;
     my $c = shift;
